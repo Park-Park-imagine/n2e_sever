47db43d6826c90aa6c5d153b5017bbd151949ed0
junseo <wawajun97@gmail.com>
2025-08-02 12:47:58 +0900

    프로젝트 생성

---
+++ src/main/java/com/example/n2eServer/N2eServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.n2eServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class N2eServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(N2eServerApplication.class, args);
+	}
+
+}
